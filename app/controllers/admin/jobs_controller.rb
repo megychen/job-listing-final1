f949c1e157c6d0b50baf5d92e86730ce62d2cc88
megy <megy@megys-MacBook-Pro.local>
2017-01-11 14:20:42 +0800

    implement admin/jobs new & create action

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -4,4 +4,23 @@ class Admin::JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.create(job_params)
+    if @job.save
+      redirect_to admin_jobs_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email)
+  end
 end
