fb6580f601a36af14c73d6620ca7add4991211d0
megy <megy@megys-MacBook-Pro.local>
2017-01-11 13:42:58 +0800

    implement jobs#index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111042854) do
+ActiveRecord::Schema.define(version: 20170111052732) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "wage_lower_bound"
+    t.integer  "wage_upper_bound"
+    t.string   "contact_email"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
