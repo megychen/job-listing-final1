505e39e3f48ace0151c7eb73cf3702cb4c1d3040
megy <megy@megys-MacBook-Pro.local>
2017-01-11 22:23:29 +0800

    user can filter job by salary & time

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,8 +3,15 @@ class JobsController < ApplicationController
 
   def index
     @jobs = Job.all.where(:is_hidden => false).recent
+    @jobs = case params[:order]
+            when "by_upper_bound"
+              Job.published.order("wage_upper_bound DESC")
+            when "by_lower_bound"
+              Job.published.order("wage_lower_bound DESC")
+            else
+              Job.published.order("created_at DESC")
+            end
   end
-
   def show
     @job = Job.find(params[:id])
     if @job.is_hidden
