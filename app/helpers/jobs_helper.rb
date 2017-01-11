daf1cfeceedafe93114e0304d51992d70f58d910
megy <megy@megys-MacBook-Pro.local>
2017-01-11 21:24:25 +0800

    render job status with icon

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,9 @@
 module JobsHelper
+  def render_job_status(job)
+    if job.is_hidden
+      content_tag(:i, "", class: "fa fa-lock")
+    else
+      content_tag(:i, "", class: "fa fa-globe")
+    end
+  end
 end
