3b73a409f4e0eb5d07db92a7706f947e1e0333ac
megy <megy@megys-MacBook-Pro.local>
2017-01-11 21:03:09 +0800

    add require_is_admin to jobs

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_is_admin
 
   def index
     @jobs = Job.all
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @jobs = Job.all
